ded3ba67158d84c85cba38d5147092e4855ba954
Pavel Pavlov <pavel.pavlov1990@gmail.com>
2024-01-27 15:26:26 +0100

    wip

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,33 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+  })
+
+  const [items, setItems] = useState([])
+
+  const handleChange = (e) => {
+    const { id, value } = e.target
+    setFormData((prevData) => ({ ...prevData, [id]: value }));
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = formData
+
+
+
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>FORM</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='name'>Name</label>
+            <input type='text' id='name' onInput={handleChange}></input>
+
+            <label htmlFor='email'>Email:</label>
+            <input id='email' type='email' onInput={handleChange}></input>
+
+            <button type='submit'>Submit</button>
+          </div>
+        </form>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
